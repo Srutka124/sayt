422d51cc5e9c51c23538a245aaec2dc0e15427c5
Srutka124 <115697052+Srutka124@users.noreply.github.com>
2023-12-23 19:34:21 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ from flask import Flask , session
 from random import randint
 from puiz_sql import get_question
 import main_2
+def start_quiz():
+    session['quiz'] = quiz_id
+    session['last_question'] = 0
 def index():
     session['counter'] = randint(0,1)
     session['counter2'] = randint(0,1)
@@ -18,5 +21,6 @@ app.config["SECRET_KEY"] = 'qweqwe123'
 app.add_url_rule('/' , "home", index)
 app.add_url_rule('/test' , "test" , test)
 app.add_url_rule('/result' , "result" , result)
+if __name__ == '__main__':
 
-app.run()
+    app.run()
--- puiz_sql.py
+++ puiz_sql.py
@@ -5,9 +5,9 @@ def get_question(quiz_id, question_id):
     conn = sqlite3.connect("quiz.splite")
     cursor = conn.cursor()
 
-    cursor.execute('''SELECT question.qestion, FROM question , quiz_content
+    cursor.execute('''SELECT question.question FROM  question , quiz_content
                    WHERE quiz_content.question_id == question.id == question.id
                    AND question.id = ?
-                   AND quiz_content.quiz_id == ?'''[question_id,quiz_id])
+                   AND quiz_content.quiz_id == ?''', [question_id,quiz_id])
     data = cursor.fetchall()
     return data
